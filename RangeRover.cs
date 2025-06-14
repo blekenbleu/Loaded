b3199889c191ec0d13f9eb6db03b1c9d52f62c6d
Steven Carmichael <blekenbleu@gmail.com>
2025-06-14 12:26:32 -0400

    discard center of gravity estimates;  calculate SwayRate as angle
    calculate MatchGain separately from SlipGain

--- RangeRover.cs
+++ RangeRover.cs
@@ -1,33 +1,54 @@
-﻿using GameReaderCommon;
-using SimHub.Plugins;
-using System;
+﻿using System;
 
 namespace blekenbleu.loaded
 {
 	public partial class Loaded // : IPlugin, IDataPlugin, IWPFSettingsV2
 	{
-        // set property names in Game(); values in DataUpdate()
-		// Call in AttachDelegate()
-        double RangeRover()
+		double MRyaw = 0, MRsway = 0;
+		double MatchRates()
 		{
-			// Assumed distances to front and rear axles in meters unavailable in game
-			var Df = 1.2;			// Distance to front axle
-			var Dr = 1.6;			// Distance to rear axle
+			if (1 < SwayAcc || -1 > SwayAcc)
+				return View.Model.MatchGain;
 
-			// Typical steering angle 24 degrees, match to what is set in game
-			double steeringRad = 24 * Math.PI/180, Rd = 180/Math.PI;
+			double ayaw = Math.Abs(YawRate);
+			double asway = Math.Abs(SwayRate);
+			if (ayaw > 20 || asway > 1)
+				return View.Model.MatchGain;
 
-			// slip angles by simplified equation
+			LPfilter(ref MRyaw, 10, YawRate);
+			LPfilter(ref MRsway, 10, SwayAcc);
+			var diff = MRyaw - MRsway;
+			if (1 > diff && -1 < diff)
+			{
+				double gain = MRyaw / MRsway;
+				if (10 < gain && 90 > gain)
+					View.Model.MatchGain = (int)(0.5 * gain);
+			}
+			return View.Model.MatchGain;
+		}
+
+		// AttachDelegate() calls this
+		// property names set in Game(); values in DataUpdate()
+		// Typical steering angle 24 degrees, match to what is set in game
+		readonly double steeringRad = 24 * Math.PI/180, Rd = 180/Math.PI;
+		double RangeRover()
+		{	// slip angles by simplified equation,  ignoring CoG
 			if (5 > SpeedKmh)
-				return 0;
+				return 0;		// not only don't care, also blows up near 0
+			SwayRate = Math.Atan(1000 * SwayAcc / SpeedKmh);
 
-			// Normalized steering input [ RANGE = -1.0f->1.0f ]
-			var steeringAngleRad = steeringRad * Steering;
-			var front_slip_angle = steeringAngleRad - Math.Atan((Vsway + Df * YawRate) / SpeedKmh);
-			var rear_slip_angle = -Math.Atan((Vsway - Dr * YawRate) / SpeedKmh);
+			// match SwayRate and YawRate for low slips
+			var SwayScaled = SwayRate * MatchRates();
+
+			var rear_slip_angle = Rd * (SwayScaled - YawRate);	// trajectory - orientation
+			// Normalized input [ -1.0f <= Steering <= 1.0f ]
+			var steeringAngle = steeringRad * Steering;
+			// front_slip_angle is difference between direction wheels point and trajectory
+			// vehicle yaw velocity adds to wheel direction
+			var front_slip_angle = Rd * (SwayScaled - (steeringAngle + YawRate));
 
 			// Oversteer in degrees
-			return Rd * (rear_slip_angle - front_slip_angle);	// positive = Oversteer
+			return Math.Abs(rear_slip_angle) - Math.Abs(front_slip_angle);	// positive = Oversteer
 		}
 	}
 }
